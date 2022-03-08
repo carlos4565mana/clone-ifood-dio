24afde105cd31f0e10b3ea2032a9fd9d3a2e3463
carlos4565mana <carloscal61@gmail.com>
2022-03-08 12:46:46 -0300

    added the dependencies , created route.js file and foled, created the Home folder with the index.js file

--- scr/AppEntry.js
+++ scr/AppEntry.js
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
-import App from './App';
 
-registerRootComponent(App);
+import Routes from './routes';
+
+registerRootComponent(Routes);
