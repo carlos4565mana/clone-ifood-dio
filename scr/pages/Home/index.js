1d5e8b53ff4aa37be9b4c9bdceb25e0cdc5a1d71
carlos4565mana <carloscal61@gmail.com>
2022-03-08 13:22:35 -0300

    add styly.js at Home

--- scr/pages/Home/index.js
+++ scr/pages/Home/index.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Image, View, ButtonContainer } from './style';
+import { Text } from '../../components/Buttom/style';
+import Button from '../../components/Buttom';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <>
+      <StatusBar style='theme-dark' />
+      <SafeAreaView>
+        <View>
+          <Text>Pedir comida nunca foi tão fácil</Text>
+          <Image source={banner} />
+          <Text>Permitir localização</Text>
+          <Text>Para descobrir restaurantes perto de vc</Text>
+        </View>
+        <ButtonContainer>
+          <Button text="PULAR"  onPress={()=>{}} />
+          <Button text="ENTRAR"  theme="primary" />
+        </ButtonContainer>
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+      </SafeAreaView>
+    
+    
+    
+    </>
+  ); }
+  
+
